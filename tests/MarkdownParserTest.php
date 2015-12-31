631ca6147d1053e4c0dcb72665036c77ed1044df
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:37:52 +1300

    Converted to PSR-2

--- tests/MarkdownParserTest.php
+++ tests/MarkdownParserTest.php
@@ -1,14 +1,15 @@
 <?php
 
-class MarkdownParserTest extends SapphireTest {
+class MarkdownParserTest extends SapphireTest
+{
 
-  function testParsingMarkdown() {
-    $text     = "**A test**\n\nAnd a new paragraph.";
-    $expected = "<p><strong>A test</strong></p><p>And a new paragraph.</p>";
-    $parser = new MarkdownParser($text);
+    public function testParsingMarkdown()
+    {
+        $text     = "**A test**\n\nAnd a new paragraph.";
+        $expected = "<p><strong>A test</strong></p><p>And a new paragraph.</p>";
+        $parser = new MarkdownParser($text);
     // we ignore wrapping whitespace and newlines 
-    $result = trim(str_replace("\n",'',$parser->parse()));
-    $this->assertEquals($result, $expected, 'Translation markdown to HTML.');
-  }
-
+    $result = trim(str_replace("\n", '', $parser->parse()));
+        $this->assertEquals($result, $expected, 'Translation markdown to HTML.');
+    }
 }
