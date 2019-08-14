8590fd6fa05e3710533c9a0914cd25e5f11c1b6d
Philipp Staender <pstaender@mailbox.org>
2019-08-14 11:10:00 +0200

    rewrite: injection, tests and parsedown

---
+++ src/SilverStripeMarkdownParser/DBStringExtension.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Zeitpulse\SilverStripeMarkdownParser;
+
+use Parsedown;
+use ParsedownExtra;
+use SilverStripe\ORM\DataExtension;
+use League\HTMLToMarkdown\HtmlConverter;
+use SilverStripe\Core\Injector\Injector;
+
+class DBStringExtension extends DataExtension
+{
+    private static $casting = [
+        'Markdown' => 'HTMLFragment',
+        'InlineMarkdown' => 'HTMLFragment',
+        'MarkdownExtra' => 'HTMLFragment',
+        'InlineMarkdownExtra' => 'HTMLFragment',
+        'HTMLToMarkdown' => 'Text',
+    ];
+
+    public function markdown(): string
+    {
+        return Injector::inst()
+            ->create(Parsedown::class)
+            ->text($this->owner->RAW());
+    }
+
+    public function markdownExtra(): string
+    {
+        return Injector::inst()
+            ->create(ParsedownExtra::class)
+            ->text($this->owner->RAW());
+    }
+
+    public function inlineMarkdown(): string
+    {
+        return Injector::inst()
+            ->create(Parsedown::class)
+            ->line($this->owner->RAW());
+    }
+
+    public function inlineMarkdownExtra(): string
+    {
+        return Injector::inst()
+            ->create(ParsedownExtra::class)
+            ->line($this->owner->RAW());
+    }
+
+    public function htmlToMarkdown(): ?string
+    {
+        return Injector::inst()
+            ->create(HtmlConverter::class)
+            ->convert($this->owner->RAW());
+    }
+}
